f194ba21a6f0fd3bc70eaad68fd7f9866eca5627
Alex Huang <u7463831@anu.edu.au>
2023-10-17 14:32:54 +1100

    Rename BarrierOneFinishTrigger

--- Hurry! The Passenger/Assets/Course Library/Scripts/Barrier 1/BarrierOneFinishTrigger.cs
+++ Hurry! The Passenger/Assets/Course Library/Scripts/Barrier 1/BarrierOneFinishTrigger.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 // A script to an invisible object located at the airport gate to check if the play has finish barrier 1
-public class BarrierOneFinishCheckpoint : MonoBehaviour
+public class BarrierOneFinishTrigger : MonoBehaviour
 {
     // Player passed through the airport gate. Means that player has complete Barrier 1
     private void OnTriggerEnter(Collider other)
